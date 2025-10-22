cafe4dbeb5221a26f000616ff98dd45f241b8434
aljon <aljonkenfernandez36@gmail.com>
2025-10-22 12:15:55 +0800

    db(migrations): update users table schema
    - Modify users table to include phone_number
    - Ensure compatibility with new user data structure and time logs

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,13 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['user', 'admin']);
-            $table->enum('active_status', ['active', 'inactive']);
             $table->enum('account_status', ['activated', 'deactivated']);
+            $table->string('phone_number')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
